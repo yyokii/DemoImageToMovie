95ea81e349ef781d549ec3488d04fa014364f771
yyokii <yyokii.h@gmail.com>
2023-08-22 09:55:21 +0900

    Initial Commit

---
+++ DemoImageToMovie/DemoImageToMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoImageToMovieApp.swift
+//  DemoImageToMovie
+//
+//  Created by Higashihara Yoki on 2023/08/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoImageToMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
